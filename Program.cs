2c11ca908930b7206971bfdab289eacfb82a4c46
Ali Salmeh <alisalmeh18@gmail.com>
2023-08-10 18:36:01 +0330

    add initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AliSalmeh.ChallengeWeek3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ChalengeNumber1
+            /* Console.WriteLine("Enter side a: ");
+            double sideA = Convert.ToDouble(Console.ReadLine());
+            double result1 = Math.Pow(sideA, 2);
+
+            Console.WriteLine("Enter side b: ");
+            double sideB = Convert.ToDouble(Console.ReadLine());
+            double result2 = Math.Pow(sideB, 2);
+
+            Console.WriteLine("Side c is: ");
+            double M = result1 + result2;
+            double sideC = Math.Sqrt(M);
+            Console.WriteLine(sideC); */
+
+            // Refactor
+            Console.WriteLine("Enter side a: ");
+            double sideA = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter side b: ");
+            double sideB = Convert.ToDouble(Console.ReadLine());
+
+            var sideC = Math.Sqrt((Math.Pow(sideA, 2) + Math.Pow(sideB, 2)));
+            Console.WriteLine($"The side c is: {sideC}");
+        }
+    }
+}
